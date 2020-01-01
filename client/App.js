c742aaa77acd2d79bf556f1ae3d7423828f9ad4b
Holius <justindgilley@gmail.com>
2020-01-01 14:57:59 -0600

    submit does not work

---
+++ client/App.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import GroceryList from "./GroceryList.js";
+import Search from "./Search.js";
+
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            groceryItem: '',
+            groceryList: ['apple', 'milk', 'cookies']
+        }
+        this.onType = this.onType.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);
+    }
+
+    onType (event) {
+        //console.log(event.target.value);
+        this.setState(
+            this.state.groceryItem = 'asfa'
+        )
+    }
+
+    onSubmit() {
+        this.setState(
+            this.state.groceryList = [this.state.groceryItem, ...this.state.groceryList]
+        )
+    }
+
+    render (props) {
+        return (
+          <div>
+            <h1> My Grocery List </h1>
+                <Search onType={this.onType} onSubmit={this.onSubmit}/>
+                <GroceryList groceryList={this.state.groceryList} /> 
+          </div> 
+            
+        )
+    }
+
+}
+
+export default App;
